93c75fe05dacf3764a44d235cb32623e728bad81
Guidarom <guidodaniel.davila@gmail.com>
2023-05-04 15:29:56 +0200

    background loging oks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 //import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
@@ -8,6 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sprint-9';
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0);
+    });
+  }
+
+
 }
 // el siguiente codigo no funciona para añadir fondo
 /* backgroundImage = 'url(' + this.sanitizer.bypassSecurityTrustUrl('../assets/resumen-superficie-texturas-muro-piedra-hormigon-blanco.jpg') + ')';
